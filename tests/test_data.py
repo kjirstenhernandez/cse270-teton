897f172631ba86035e79411eaa32fb3205f1a1ba
Kjirsten Hernandez <sandlot7665@gmail.com>
2025-02-16 02:16:46 -0700

    updated smoke test (fixed errors)

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,12 +1,14 @@
 import pytest
 import requests
 
+
 @pytest.fixture
 def mock_requests_get(mocker):
-    response_json = {"businesses":[{"name":"Teton Elementary"}]}
-    
+    response_json = {"businesses": [{"name": "Teton Elementary"}]}
+
     # Patching requests.get to return a mocked response
     mocker.patch.object(requests, 'get', return_value=MockResponse(response_json))
+
 
 class MockResponse:
     def __init__(self, json_data, status_code=200):
@@ -15,6 +17,7 @@ class MockResponse:
 
     def json(self):
         return self.json_data
+
 
 def test_data_endpoint(mock_requests_get):
     # Make the HTTP GET request
